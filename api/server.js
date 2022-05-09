6b9b8cb9aa33cea5543b74bd044800091529f8c7
enhopkin@gmail.com <enhopkin@gmail.com>
2022-05-09 15:13:25 -0600

    mvp complete

--- api/server.js
+++ api/server.js
@@ -61,4 +61,25 @@ server.delete('/api/users/:id', (req, res) => {
     })
 })
 
+server.put('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    const {name, bio} = req.body
+    if(!name || !bio) {
+        res.status(400).json({message:'Please provide name and bio for the user'})
+    }else{
+        Users.update(id,req.body)
+        .then(result => {
+            if(!result){
+                res.status(404).json({message:'The user with the specified ID does not exist'})
+            }else{
+                res.json(result)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({message: 'The user information could not be modified'})
+        })
+    }
+    })
+
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
